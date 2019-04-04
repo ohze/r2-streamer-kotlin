cf08d910d4024e86e8855d8253dd7c9324590f2c
Bùi Việt Thành <thanhbv@sandinh.net>
2019-04-04 17:32:52 +0700

    clean code of DrmDecoder

--- r2-streamer/src/main/java/org/readium/r2/streamer/fetcher/DrmDecoder.kt
+++ r2-streamer/src/main/java/org/readium/r2/streamer/fetcher/DrmDecoder.kt
@@ -11,7 +11,6 @@ package org.readium.r2.streamer.fetcher
 
 import org.readium.r2.shared.drm.Drm
 import org.readium.r2.shared.Link
-import timber.log.Timber
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
@@ -19,48 +18,31 @@ import java.util.zip.Inflater
 
 class DrmDecoder {
     fun decoding(input: InputStream, resourceLink: Link, drm: Drm?): InputStream {
+        if (drm == null) return input
 
-        val scheme = resourceLink.properties.encryption?.scheme?.let {
-            return@let it
-        } ?: run {
-            return input
+        val scheme = resourceLink.properties.encryption?.scheme ?: return input
+
+        if (scheme != drm.scheme) return input
+
+        var data = decipher(input, drm) ?: return input
+
+        if (resourceLink.properties.encryption?.compression == "deflate") {
+            val padding = data[data.size - 1].toInt()
+            data = Inflater(true).inflateAll(data, 0, data.size - padding)
         }
+        return ByteArrayInputStream(data)
+    }
 
-        drm?.let {
-            if (scheme == drm.scheme) {
-                var data = decipher(input, drm) ?: return input
-
-                if (resourceLink.properties.encryption?.compression == "deflate") {
-
-                    val padding = data[data.size - 1].toInt()
-                    data = data.copyOfRange(0, data.size - padding)
-                    val inflater = Inflater(true)
-                    inflater.setInput(data)
-                    val output = ByteArrayOutputStream(data.size)
-                    val buf = ByteArray(1024)
-                    while (!inflater.finished()) {
-                        try {
-                            val count = inflater.inflate(buf)
-                            output.write(buf, 0, count)
-                        } catch (e: Exception) {
-                            Timber.e(e)
-                        }
-                    }
-                    try {
-                        output.close()
-                    } catch (e: Exception) {
-                        Timber.e(e)
-                    }
-                    data = output.toByteArray()
-                    
-                }
-                return ByteArrayInputStream(data)
-            }
-            return input
-
-        } ?: run {
-            return input
+    private fun Inflater.inflateAll(data: ByteArray, off: Int, len: Int): ByteArray {
+        setInput(data, off, len)
+        val buf = ByteArray(1024)
+        val output = ByteArrayOutputStream(len)
+        while (!finished()) {
+            val count = inflate(buf)
+            output.write(buf, 0, count)
         }
+        // Closing a ByteArrayOutputStream has no effect
+        return output.toByteArray()
     }
 
     private fun decipher(input: InputStream, drm: Drm): ByteArray? {
